dca32958c1ba8cfd414493c0193d0a7f116d50b3
SafwanFaruk <safwan.faruk17>
2024-12-28 22:18:41 +0600

    operator

---
+++ operator.py
@@ -0,0 +1,15 @@
+a1=100
+a2=2
+
+print("addition",a1+a2)
+print("subtraction",a1-a2)
+print("multiplication",a1*a2)
+print("division",a1/a2)
+print("floor",a1//a2)
+print("modules",a1%a2)
+print("expo",a1**a2)
+
+print(a1==a2)
+print(a1!=a2)
+print(a1>=a2)
+print(a1<=a2)
---
+++ w.py
@@ -0,0 +1,9 @@
+name="Safwan"
+age=15
+weight=51
+
+print(name,type(name))
+print(age,type(age))
+print(weight,type(weight))
+age=str,(15)
+print(age)
